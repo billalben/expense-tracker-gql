7fa524487a9d29cbf3915aaef6ae2eb0107cebe3
Benzazoua <billalben252@gmail.com>
2024-09-29 13:00:50 +0100

    Transaction Resolver

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,6 +1,109 @@
+import Transaction from "../models/transaction.model.js";
+
 const transactionResolver = {
-  Mutation: {},
-  Query: {},
+  Query: {
+    transactions: async (_, __, context) => {
+      try {
+        if (!context.getUser()) throw new Error("Unauthorized");
+        const userId = await context.getUser()._id;
+
+        const transactions = await Transaction.find({ userId });
+        return transactions;
+      } catch (error) {
+        throw new Error("Error getting transactions");
+      }
+    },
+    transaction: async (_, { transactionId }) => {
+      try {
+        const transaction = await Transaction.findById(transactionId);
+        return transaction;
+      } catch (error) {
+        throw new Error("Error getting transaction");
+      }
+    },
+    categoryStatistics: async (_, __, context) => {
+      if (!context.getUser()) throw new Error("Unauthorized");
+
+      const userId = context.getUser()._id;
+      const transactions = await Transaction.find({ userId }).select(
+        "category amount"
+      );
+      const categoryMap = {};
+
+      /**
+       * transactions how it looks like
+       * const transactions = [
+          { category: "expense", amount: 50 },
+          { category: "expense", amount: 75 },
+          { category: "investment", amount: 100 },
+          { category: "saving", amount: 30 },
+          { category: "saving", amount: 20 }
+        ];
+      */
+
+      transactions.forEach((transaction) => {
+        if (!categoryMap[transaction.category]) {
+          categoryMap[transaction.category] = 0;
+        }
+        categoryMap[transaction.category] += transaction.amount;
+      });
+
+      /**
+       * categoryMap how it looks like
+       * categoryMap = { expense: 125, investment: 100, saving: 50 }
+       */
+
+      return Object.entries(categoryMap).map(([category, totalAmount]) => ({
+        category,
+        totalAmount,
+      }));
+
+      /**
+       * Object.entries(categoryMap) how it looks like
+       * return [ { category: "expense", totalAmount: 125 }, { category: "investment", totalAmount: 100 }, { category: "saving", totalAmount: 50 } ]
+       */
+    },
+  },
+  Mutation: {
+    createTransaction: async (_, { input }, context) => {
+      try {
+        if (!context.getUser()) throw new Error("Unauthorized");
+
+        const newTransaction = await Transaction.create({
+          ...input,
+          userId: context.getUser()._id,
+        });
+
+        return newTransaction;
+      } catch (error) {
+        throw new Error("Error creating transaction");
+      }
+    },
+    updateTransaction: async (_, { input }) => {
+      try {
+        const updatedTransaction = await Transaction.findByIdAndUpdate(
+          input.transactionId,
+          input,
+          {
+            new: true,
+          }
+        );
+        return updatedTransaction;
+      } catch (error) {
+        throw new Error("Error updating transaction");
+      }
+    },
+    deleteTransaction: async (_, { transactionId }) => {
+      try {
+        const deletedTransaction = await Transaction.findByIdAndDelete(
+          transactionId
+        );
+        return deletedTransaction;
+      } catch (error) {
+        throw new Error("Error deleting transaction");
+      }
+    },
+  },
 };
 
 export default transactionResolver;
