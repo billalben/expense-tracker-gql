e291e97eb6aa9567dd41b998dc87f2a62229f901
Benzazoua <billalben252@gmail.com>
2024-09-29 21:34:33 +0100

    SignUp & Login Components

---
+++ frontend/src/components/RadioButton.tsx
@@ -0,0 +1,53 @@
+type RadioButtonProps = {
+  id: string;
+  label: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+  checked: boolean;
+  name?: string;
+};
+
+const RadioButton = ({
+  id,
+  label,
+  onChange,
+  value,
+  checked,
+}: RadioButtonProps) => {
+  return (
+    <div className="inline-flex items-center">
+      <label
+        className="relative flex cursor-pointer items-center rounded-full p-3"
+        htmlFor={id}
+      >
+        <input
+          name="type"
+          type="radio"
+          className="before:content[''] before:bg-blue-gray-500 peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-black text-gray-900 transition-all before:absolute before:left-2/4 before:top-2/4 before:block before:h-12 before:w-12 before:-translate-x-2/4 before:-translate-y-2/4 before:rounded-full before:opacity-0 before:transition-opacity checked:border-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
+          id={id}
+          value={value}
+          onChange={onChange}
+          checked={checked}
+        />
+        <span className="pointer-events-none absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 text-gray-900 opacity-0 transition-opacity peer-checked:opacity-100">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-3.5 w-3.5"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+          >
+            <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
+          </svg>
+        </span>
+      </label>
+      <label
+        className="mt-px cursor-pointer select-none font-light text-gray-700"
+        htmlFor={id}
+      >
+        {label}
+      </label>
+    </div>
+  );
+};
+
+export default RadioButton;
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,5 +1,87 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import InputField from "../components/InputField";
+import toast from "react-hot-toast";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: "",
+  });
+  const loading = false;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setLoginData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!loginData.username || !loginData.password)
+      return toast.error("Please fill in all fields");
+    try {
+      // Call login API here
+      console.log(loginData);
+    } catch (error) {
+      // toast.error(error.message);
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="z-50 flex overflow-hidden rounded-lg bg-gray-300">
+        <div className="flex w-full min-w-80 items-center justify-center bg-gray-100 sm:min-w-96">
+          <div className="w-full max-w-md p-6">
+            <h1 className="mb-6 text-center text-3xl font-semibold text-black">
+              Login
+            </h1>
+            <h1 className="mb-6 text-center text-sm font-semibold text-gray-500">
+              Welcome back! Log in to your account
+            </h1>
+            <form className="space-y-4" onSubmit={handleSubmit}>
+              <InputField
+                label="Username"
+                id="username"
+                name="username"
+                value={loginData.username}
+                onChange={handleChange}
+              />
+
+              <InputField
+                label="Password"
+                id="password"
+                name="password"
+                type="password"
+                value={loginData.password}
+                onChange={handleChange}
+              />
+              <div>
+                <button
+                  type="submit"
+                  className="w-full rounded-md bg-black p-2 text-white transition-colors duration-300 hover:bg-gray-800 focus:bg-black focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                  disabled={loading}
+                >
+                  {loading ? "Loading..." : "Login"}
+                </button>
+              </div>
+            </form>
+            <div className="mt-4 text-center text-sm text-gray-600">
+              <p>
+                {"Don't"} have an account?{" "}
+                <Link to="/signup" className="text-black hover:underline">
+                  Sign Up
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
