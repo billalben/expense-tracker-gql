1bddc92ad757ca42877416519d8644729df90d17
Benzazoua <billalben252@gmail.com>
2024-10-05 18:32:40 +0100

    cron js added
    - prevent server (render free plan) go sleeping
    - add favicon

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,10 +7,11 @@ import GridBackground from "./components/ui/GridBackgroun.tsx";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri:
-    import.meta.env.VITE_NODE_ENV === "production"
-      ? "/graphql"
-      : "http://localhost:4000/graphql",
+  // uri:
+  //   import.meta.env.VITE_NODE_ENV === "production"
+  //     ? "/graphql"
+  //     : "http://localhost:4000/graphql",
+  uri: "/graphql",
   cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
   credentials: "include", // This tells Apollo Client to send cookies along with every request to the server.
 });
